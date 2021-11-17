bd9dd3449087d01f949c1c9b609077b7d84bd268
KhuajaShams <ksham004@ucr.edu>
2021-11-16 19:31:05 -0800

    lab6

---
+++ lab6/steps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+yum -y install httpd
+
+#vi /etc/httpd/conf/httpd.conf
+
+#systemctl start httpd.service
+
+#vi /var/www/html/index.html
+
+#yum -y install elinks
+
+#passwd root
+#useradd ksham004
+#passwd ksham004
+
+#cd /var/www/html
+#ln -s /etc/passwd password
+#elinks http://127.0.0.1/password
+
+#vi /etc/httpd/conf/httpd.conf
+# comment out FollowSymLinks
+#stop httpd service
+#restart it
+
+#systemctl enable httpd
+#uptime; systemctl status httpd.service; systemctl list-units httpd.service
+
+
+#yum -y install php php-mysql
+
+#vi /var/www/html/index.php
+
+#INSERT MYSQL DOWNLOAD COMMANDS HERE
